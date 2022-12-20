fa290aa0a9681ca066ace01822488637a0192e67
xnikma <xnikma@gmail.com>
2022-12-20 12:31:32 +0100

    first commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var reactCircleCardF1520DF6DA8B4310BB87C276528187B8_DEBUG: IVisualPlugin;
+export default reactCircleCardF1520DF6DA8B4310BB87C276528187B8_DEBUG;
---
+++ README.md
@@ -0,0 +1,1 @@
+"# PBIViz-React" 
